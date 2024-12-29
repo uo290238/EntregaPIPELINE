fb77a598754b4ffe94457b52dae0eb60e0e7b3d5
isabel <isabeltejedor03@gmail.com>
2024-12-29 13:40:41 +0100

    Cambios finales

--- testDistancia.py
+++ testDistancia.py
@@ -8,7 +8,7 @@ class TestDistanciaEuclidea(unittest.TestCase):
         p1 = (1, 2)
         p2 = (4, 6)
         resultado = distancia_euclidea_2d(p1, p2)
-        self.assertEqual(resultado, 5)  # Sabemos que la distancia entre (1, 2) y (4, 6) es 5
+        self.assertEqual(resultado, 5)
 
 if __name__ == '__main__':
     unittest.main()
