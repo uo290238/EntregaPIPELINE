b670f8b65b4820d7135c575b69e34c1c6f0c9db5
isabel <isabeltejedor03@gmail.com>
2024-12-29 13:43:10 +0100

    Agregar workflow para pruebas 2

--- distancia.py
+++ distancia.py
@@ -3,7 +3,3 @@ import math
 def distancia_euclidea_2d(p1, p2):
     return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
-if __name__ == '__main__':
-    p1 = (1, 2)
-    p2 = (4, 6)
-    print(f"Distancia Euclidiana en 2D: {distancia_euclidea_2d(p1, p2)}")
